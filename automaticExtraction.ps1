c749a346eb3155f46e9dd38ee6c40f9f5804f3c3
ysalexg <alexgcord@proton.me>
2025-03-18 22:41:43 -0300

    init repo

---
+++ automaticExtraction.ps1
@@ -0,0 +1,133 @@
+# Directorios de trabajo
+$downloadFolder = "E:\Descargas"
+$outputFolder = "D:\Extracciones"
+
+# Ruta a handle.exe
+$handlePath = "D:\Programacion\Path\handle.exe"
+
+# Función para verificar si un archivo está en uso por IDMan.exe
+function Is-FileInUseByIDMan {
+    param (
+        [string]$filePath
+    )
+
+    $handleOutput = & $handlePath $filePath
+    return $handleOutput -match "IDMan.exe"
+}
+
+# Función para verificar si un archivo está en uso por Hydra.exe
+function Is-FileInUseByHydra {
+    param (
+        [string]$filePath
+    )
+
+    $handleOutput = & $handlePath $filePath
+    return $handleOutput -match "aria2c.exe"
+}
+
+# Función para verificar y extraer archivos
+function Extract-Files {
+    $filesToExtract = Get-ChildItem -Path $downloadFolder -Include *.rar, *.zip, *.7z -File -Recurse
+    foreach ($archive in $filesToExtract) {
+        $archiveName = [System.IO.Path]::GetFileNameWithoutExtension($archive.FullName)
+        $extractionPath = Join-Path -Path $outputFolder -ChildPath $archiveName
+
+        # Crear carpeta para la extracción
+        if (-not (Test-Path -Path $extractionPath)) {
+            New-Item -Path $extractionPath -ItemType Directory | Out-Null
+        }
+
+        # Esperar hasta que el archivo no esté en uso ni por IDMan.exe ni por Hydra.exe
+        Write-Output "Verificando si $($archive.FullName) esta en uso por IDM o Hydra..."
+        while (Is-FileInUseByIDMan -filePath $archive.FullName -or Is-FileInUseByHydra -filePath $archive.FullName) {
+            Write-Output "Archivo en uso por IDM o Hydra. Esperando..."
+            Start-Sleep -Seconds 5
+        }
+
+        # Verificar si el archivo fue usado por Hydra.exe
+        if (Is-FileInUseByHydra -filePath $archive.FullName) {
+            Write-Output "Archivo usado por Hydra. Esperando a que termine de descargar..."
+            while (Is-FileInUseByHydra -filePath $archive.FullName) {
+                Write-Output "Archivo aun en uso por Hydra. Esperando..."
+                Start-Sleep -Seconds 5
+                cls
+            }
+            cls
+            Write-Output "Archivo ya no esta en uso por Hydra. Ejecutando instalador..."
+            
+            if ($archive.Name -like "*SteamRIP*") {
+                Write-Output "Archivo contiene 'SteamRIP'. Ejecutando instalador..."
+                Start-Process -FilePath "python" -ArgumentList "`"D:\Programacion\Python\Automatic Game Instalation\ui.py`"" -NoNewWindow -Wait
+                Write-Output "ui.py ejecutado para $($archive.FullName)"
+            }
+        } else {
+            Write-Output "Extrayendo $($archive.FullName) a $extractionPath..."
+            $arguments = "x `"$($archive.FullName)`" -o`"$extractionPath`" -aoa"
+            Start-Process -FilePath "7z.exe" -ArgumentList $arguments -NoNewWindow -Wait
+
+            # Eliminar archivo después de la extracción
+            Remove-Item -Path $archive.FullName -Force
+            Write-Output "Archivo eliminado: $($archive.FullName)"
+        }
+    }
+}
+
+
+
+# Función para cerrar IDMan.exe
+function Close-IDMan {
+    if (Get-Process -Name "IDMan" -ErrorAction SilentlyContinue) {
+        Write-Output "Cerrando IDM..."
+        Stop-Process -Name "IDMan" -Force
+        Start-Sleep -Seconds 1  # Dar tiempo a que el proceso se cierre
+        cls  # Limpiar la pantalla de la consola
+    }
+}
+
+# Función para verificar si IDMan.exe está en ejecución
+function Is-IDManRunning {
+    return Get-Process -Name "IDMan" -ErrorAction SilentlyContinue
+}
+
+
+# Función para verificar si Hydra.exe está en ejecución
+function Is-HydraRunning {
+    return Get-Process -Name "Hydra" -ErrorAction SilentlyContinue
+}
+
+# Bucle principal
+while ($true) {
+    # Esperar a que IDMan.exe esté en ejecución o que un archivo esté en uso por Hydra.exe
+    Write-Output "Esperando a que IDM este en ejecucion o que un archivo este en uso por Hydra..."
+
+    # Aquí se verifica si IDMan.exe está en ejecución o si algún archivo está siendo manejado por Hydra.exe
+    while (-not (Is-IDManRunning) -and (Get-ChildItem -Path $downloadFolder -Include *.rar, *.zip, *.7z -File -Recurse | Where-Object { Is-FileInUseByHydra -filePath $_.FullName }).Count -eq 0) {
+        Start-Sleep -Seconds 5
+    }
+
+    Write-Output "IDM o archivo en uso por Hydra detectado. Comenzando la extraccion de archivos..."
+
+    # Verificar si hay archivos para extraer
+    $filesToExtract = Get-ChildItem -Path $downloadFolder -Include *.rar, *.zip, *.7z -File -Recurse
+
+    if ($filesToExtract.Count -gt 0) {
+        # Extraer archivos
+        Extract-Files
+
+        # Cerrar IDMan.exe si está en ejecución
+        Close-IDMan
+
+        # Esperar a que IDMan.exe o Hydra.exe se vuelva a ejecutar
+        Write-Output "Esperando a que IDM o Hydra se vuelvan a iniciar..."
+        while (-not (Is-IDManRunning) -and -not (Is-HydraRunning)) {
+            Start-Sleep -Seconds 5
+        }
+
+        Write-Output "IDM o Hydra detectados nuevamente. Continuando con la siguiente verificacion..."
+    } else {
+        Write-Output "No hay archivos para extraer."
+    }
+
+    # Esperar un intervalo antes de la próxima verificación
+    Start-Sleep -Seconds 5
+}
---
+++ ui.py
@@ -0,0 +1,459 @@
+# ENHANCEMENTS
+# Integrar DODI (cant) - FitGirl - Xatab - Kaos - GOG - Generic
+# Subir porcentaje de acuerdo a porcentaje de extracción
+# Si no encuentra ejecutable -> buscar por nombre en steamdb -> usar config para determinar el .exe
+# Sino, si no encuentra ejecutable -> buscar todos los .exe en steamdb -> si encuentra coincidencia, usar appid encontrado para el resto
+
+# Al terminar development -> descomentar os.remove (borrar solo si es exitoso) y el cerrado automatico (cerrar solo si es exitoso)
+
+import os
+import sys
+import requests
+import shutil
+import subprocess
+from pathlib import Path
+from ruamel.yaml import YAML
+import subprocess
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QVBoxLayout, QLabel, 
+                             QProgressBar, QWidget, QTextEdit, QDesktopWidget, QPushButton)
+from PyQt5.QtCore import QThread, pyqtSignal, Qt, QPoint, QTimer
+from PyQt5.QtGui import QFont, QIcon
+
+# Variables globales
+download_folder = "E:\\Descargas"
+extraction_folder = "D:\\Extracciones"
+game_folder = "D:\\Juegos"
+manifest_url = "https://raw.githubusercontent.com/mtkennerly/ludusavi-manifest/refs/heads/master/data/manifest.yaml"
+manifest_path = "manifest.yaml"
+
+excluded_folders = [
+    r"D:\Extracciones\Drive Cache",
+    r"D:\Extracciones\Free Download Manager",
+    r"D:\Extracciones\IDM Temporal"
+]
+
+log_messages = []
+extracted_paths = []
+
+def resource_path(relative_path):
+    """Obtiene la ruta absoluta del recurso, funciona tanto para desarrollo como para el ejecutable"""
+    try:
+        # PyInstaller crea una carpeta temporal y almacena la ruta en _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+# Descargar manifest.yaml
+def download_manifest(update_progress):
+    try:
+        update_progress(10, "Descargando base de datos...")
+        print("Descargando manifest...")
+        response = requests.get(manifest_url, headers={'User-Agent': 'Mozilla/5.0'})
+        response.raise_for_status()
+        with open(manifest_path, 'wb') as file:
+            file.write(response.content)
+        print("\033[32mManifest descargado correctamente.\033[0m")
+        log_messages.append("\033[32mManifest descargado correctamente.\033[0m")
+    except Exception as e:
+        error_msg = f"Error al descargar el manifest: {e}"
+        update_progress(0, f"Error al descargar el manifest: {e}")
+        print(error_msg)
+        log_messages.append(error_msg)
+        raise
+
+# Verificar si una carpeta está excluida
+def is_excluded(folder_path):
+    return any(folder_path.startswith(excluded) for excluded in excluded_folders)
+
+def extract_archives(update_progress):
+    try:
+        def extract_recursive(file_path, destination_folder):
+            # Extrae un archivo comprimido y verifica si hay otros archivos comprimidos dentro
+            os.makedirs(destination_folder, exist_ok=True)
+            subprocess.run(["7z", "x", file_path, f"-o{destination_folder}", "-aoa"], check=True)
+            for root, dirs, files in os.walk(destination_folder):
+                for file in files:
+                    if file.endswith(('.zip', '.rar', '.7z')):
+                        nested_archive = os.path.join(root, file)
+                        nested_destination = os.path.join(destination_folder, Path(file).stem)
+                        extract_recursive(nested_archive, nested_destination)
+                        os.remove(nested_archive)  # Opcional: elimina el archivo comprimido anidado
+
+        # Usamos os.walk para recorrer todo el download_folder recursivamente
+        compressed_files = []
+        for root, dirs, files in os.walk(download_folder):
+            for file in files:
+                if file.endswith(('.zip', '.rar', '.7z')):
+                    compressed_files.append(os.path.join(root, file))
+
+        progress_step = 40 // max(len(compressed_files), 1)
+        progress = 20
+
+        for file in compressed_files:
+            extraction_path = os.path.join(extraction_folder, Path(file).stem)
+            if is_excluded(extraction_path):
+                continue
+            extracted_paths.append(extraction_path)
+            update_progress(progress, f"Extrayendo archivos...")
+            extract_recursive(file, extraction_path)
+            os.remove(file)  # Opcional: elimina el archivo original después de la extracción
+            progress += progress_step
+
+        log_messages.append(f"\033[32mArchivos extraídos correctamente.\033[0m")
+    except Exception as e:
+        error_msg = f"Error durante la extracción: {e}"
+        update_progress(0, error_msg)
+        print(error_msg)
+        log_messages.append(error_msg)
+        raise
+
+# Eliminar las carpetas de extracción específicas   
+def cleanup_extraction_paths(update_progress):
+    try:
+        for path in extracted_paths:
+            if os.path.exists(path):
+                shutil.rmtree(path)
+        update_progress(100, "Instalación completada.")
+    except Exception as e:
+        update_progress(0, f"Error al limpiar las carpetas: {e}")
+
+# Leer manifest.yaml
+def load_manifest():
+    yaml = YAML(typ="safe")
+    with open(manifest_path, "r", encoding="utf-8") as f:
+        return yaml.load(f)
+
+def process_games(update_progress):
+    try:
+        print("Cargando manifest...")
+        # log_messages.append("Cargando manifest...")
+        update_progress(85, "Cargando base de datos...")
+        manifest_data = load_manifest()
+        log_messages.append("\033[32mManifest cargado correctamente.\033[0m")
+        extracted_folders = [
+            f.path for f in os.scandir(extraction_folder)
+            if f.is_dir() and not is_excluded(f.path)
+        ]
+        excluded_executables = [
+        "dotNetFx40_Full_setup.exe",
+        "dxwebsetup.exe",
+        "oalinst.exe",
+        "vcredist_2015-2019_x64.exe",
+        "vcredist_2015-2019_x86.exe",
+        "vcredist_x64.exe",
+        "vcredist_x86.exe",
+        "xnafx40_redist.msi"
+        ]
+
+        for folder in extracted_folders:
+            print(f"Procesando carpeta: {folder}")
+            # log_messages.append(f"Procesando carpeta: {folder}")
+
+            executables = []  # Lista para almacenar los ejecutables encontrados
+
+            for root, dirs, files in os.walk(folder):
+                if is_excluded(root):
+                    continue
+                for file in files:
+                    if file.endswith(".exe") and file not in excluded_executables:
+                        executables.append((file, root))
+
+            if not executables:
+                no_exec_msg = f"No se encontró ejecutable en: {folder}"
+                print(no_exec_msg)
+                log_messages.append(no_exec_msg)
+                continue
+
+            # Procesar cada ejecutable encontrado
+            for exe_file, exe_path in executables:
+                # Si process_executable devuelve True, detener el procesamiento de más ejecutables
+                if process_executable(exe_file, exe_path, manifest_data, update_progress):
+                    print(f"Procesamiento exitoso, deteniendo búsqueda en {folder}.")
+                    break  # Salir del bucle de ejecutables
+    except Exception as e:
+        error_msg = f"Error procesando juegos: {e}"
+        print(error_msg)
+        log_messages.append(error_msg)
+
+def save_full_executable_path(target_folder, matching_exe, output_file="full_executable_path.txt"):
+    # Ruta específica donde quieres guardar el archivo
+    output_directory = r"D:\Programacion\Python\Automatic Game Instalation"
+    output_file_path = os.path.join(output_directory, output_file)
+
+    try:
+        # Buscar el ejecutable dentro de target_folder
+        for root, _, files in os.walk(target_folder):
+            if matching_exe in files:
+                full_path = os.path.join(root, matching_exe)
+                # Crear el directorio si no existe
+                os.makedirs(output_directory, exist_ok=True)
+                # Guardar el archivo en la ruta deseada
+                with open(output_file_path, "w", encoding="utf-8") as file:
+                    file.write(full_path)
+                print(f"Ruta completa del ejecutable guardada en {output_file_path}: {full_path}")
+                return
+        print(f"No se encontró el ejecutable {matching_exe} en {target_folder}.")
+    except Exception as e:
+        print(f"Error al guardar la ruta completa del ejecutable: {e}")
+
+def process_executable(executable, folder_path, manifest_data, update_progress):
+    exe_files = [f for f in os.listdir(folder_path) if f.endswith(".exe")]
+
+    for game_name, game_info in manifest_data.items():
+        launch_info = game_info.get("launch", {})
+        install_dir = list(game_info.get("installDir", {}).keys())[0] if "installDir" in game_info else None
+        steam_info = game_info.get("steam", {})
+        app_id = steam_info.get("id", None)
+
+        for launch_path in launch_info.keys():
+            # Obtener solo el nombre del archivo en launch_path (después del último "/")
+            base_exe_name = os.path.basename(launch_path)
+
+            # Encuentra el ejecutable correcto basado en el nombre completo
+            matching_exe = next((exe for exe in exe_files if exe.lower() == base_exe_name.lower()), None)
+
+            if matching_exe:
+                base_dir = install_dir or game_name
+                target_folder = os.path.join(game_folder, base_dir)
+                executable_msg = f"\033[32mEjecutable encontrado: {matching_exe}\033[0m"
+                
+                # Ruta del directorio de salida
+                output_directory = r"D:\Programacion\Python\Automatic Game Instalation"
+                os.makedirs(output_directory, exist_ok=True)
+
+                # Guardar el nombre del ejecutable
+                executable_file_path = os.path.join(output_directory, "executable.txt")
+                print(executable_msg)
+                with open(executable_file_path, "w", encoding="utf-8") as file:
+                    file.write(matching_exe)
+                
+                log_messages.append(executable_msg)
+                
+                if not os.path.exists(target_folder):
+                    move_msg = f"Moviendo {folder_path} a {target_folder}..."
+                    save_game_name(Path(target_folder).name)
+                    print(move_msg)
+                    mov_msg = f"\033[32mMovido {folder_path} a {target_folder}\033[0m"
+                    log_messages.append(mov_msg)
+                    
+                    # Guardar la ruta del juego en game_path.txt
+                    game_path_file_path = os.path.join(output_directory, "game_path.txt")
+                    with open(game_path_file_path, "w", encoding="utf-8") as file:
+                        file.write(target_folder)
+                    
+                    shutil.move(folder_path, target_folder)
+                else:
+                    print(f"La carpeta destino ya existe: {target_folder}")
+
+                if app_id:
+                    save_full_executable_path(target_folder, matching_exe)
+                else:
+                    print(f"No se encontró AppID para el juego {game_name}")
+
+                return True  # Éxito: detener búsqueda en esta carpeta
+
+    print(f"No se encontró información en el manifest para ningún ejecutable en {folder_path}.")
+    return False  # No se encontró coincidencia
+
+
+
+def save_game_name(folder_name, output_file="game_name.txt"):
+    # Definir el directorio de salida
+    output_directory = r"D:\Programacion\Python\Automatic Game Instalation"
+    output_file_path = os.path.join(output_directory, output_file)
+
+    try:
+        # Crear el directorio si no existe
+        os.makedirs(output_directory, exist_ok=True)
+        
+        # Guardar el nombre del juego en la ruta especificada
+        with open(output_file_path, "w", encoding="utf-8") as file:
+            file.write(folder_name)
+        # Agregar mensaje al log además de imprimirlo
+        mensaje = f"\033[32mNombre del juego guardado en {output_file_path}.\033[0m"
+        print(mensaje)
+        log_messages.append(mensaje)
+    except Exception as e:
+        error_msg = f"Error al guardar el nombre del juego: {e}"
+        print(error_msg)
+        log_messages.append(error_msg)
+
+
+class GameInstallationThread(QThread):
+    progress_update = pyqtSignal(int)
+    status_update = pyqtSignal(str)
+    installation_complete = pyqtSignal()
+
+    def run(self):
+        try:
+            def update_progress(progress, message):
+                self.status_update.emit(message)
+                self.progress_update.emit(progress)
+
+            download_manifest(update_progress)
+            extract_archives(update_progress)
+            process_games(update_progress)
+            cleanup_extraction_paths(update_progress)
+
+            self.installation_complete.emit()
+        except Exception as e:
+            self.status_update.emit(f"Error crítico: {str(e)}")
+
+class GameInstallationProgress(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Instalación de Juego")
+        self.setGeometry(100, 100, 255, 225)
+        
+        # Quitar barra de ventana
+        self.setWindowFlags(Qt.Window | Qt.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint | Qt.WindowType.Tool)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+        # Centrar la ventana en la pantalla
+        screen = QDesktopWidget().screenGeometry()
+        window_size = self.geometry()
+        x = (screen.width() - window_size.width()) // 2
+        y = (screen.height() - window_size.height()) // 2
+        self.move(x, y)
+
+        # Crear un contenedor central personalizado
+        central_widget = QWidget()
+        central_widget.setObjectName("central_container")
+        central_widget.setStyleSheet("""
+            #central_container {
+                background-color: #202020; 
+                border-radius: 6px;
+                border: 1px solid #F3F3F3;
+            }
+        """)
+        
+        self.setCentralWidget(central_widget)
+        layout = QVBoxLayout(central_widget)
+        
+        # Aplicar paleta de colores
+        self.setStyleSheet("""
+            QMainWindow {
+                background-color: #202020; /* Fondo oscuro */
+                border-radius: 6px; /* Redondeo de bordes */
+            }
+            QLabel {
+                color: #D8DEE9; /* Texto claro */
+            }
+            QProgressBar {
+                background-color: #2D2D2D; /* Fondo de la barra */
+                color: #88C0D0; /* Texto */
+                border-radius: 6px;
+            }
+            QProgressBar::chunk {
+                background-color: #F3F3F3; /* Color del progreso */
+                border-radius: 6px;
+            }
+            QTextEdit {
+                background-color: #2D2D2D; /* Fondo de texto */
+                color: #D8DEE9; /* Texto */
+                font-size: 12px;
+                font-family: 'Segoe UI', sans-serif;
+                border: 1px solid #202020;
+                border-radius: 6px;
+            }
+        """)
+
+        self.setAttribute(Qt.WA_StyledBackground)
+
+        # Título estilizado
+        self.title = QLabel("Instalando...")
+        self.title.setFont(QFont('Segoe UI', 16, QFont.Bold))
+        self.title.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.title)
+        
+        # Estado del progreso
+        self.status_label = QLabel("Iniciando instalación...")
+        self.status_label.setFont(QFont('Segoe UI', 12))
+        self.status_label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.status_label)
+        
+        # Barra de progreso estilizada
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setFixedHeight(25)
+        self.progress_bar.setTextVisible(False)
+        layout.addWidget(self.progress_bar)
+        
+        # Área de log de mensajes estilizada
+        self.log_text = QTextEdit()
+        self.log_text.setReadOnly(True)
+        layout.addWidget(self.log_text)
+
+        self.finish_button = QPushButton("Finalizar")
+        self.finish_button.setFixedHeight(30)
+        self.finish_button.setStyleSheet("""
+            QPushButton {
+                background-color: #F3F3F3;
+                color: #202020;
+                border-radius: 6px;
+                font-weight: bold;
+                font-family: 'Segoe UI', sans-serif;
+                font-size: 14px;
+            }
+            QPushButton:hover {
+                background-color: #D8DEE9;
+            }
+        """)
+        self.finish_button.clicked.connect(lambda: sys.exit(0))
+        self.finish_button.setVisible(False)  # Inicialmente oculto
+        layout.addWidget(self.finish_button)    
+
+        self.is_dragging = False
+        self.drag_position = QPoint()
+        
+        # Hilo de instalación
+        self.installation_thread = GameInstallationThread()
+        self.installation_thread.progress_update.connect(self.update_progress)
+        self.installation_thread.status_update.connect(self.update_status)
+        self.installation_thread.installation_complete.connect(self.on_installation_complete)
+        
+        
+        self.installation_thread.start()
+
+        self.close_timer = QTimer(self)
+        self.close_timer.setSingleShot(True)
+        self.close_timer.timeout.connect(self.close)
+
+    def update_progress(self, value):
+        self.progress_bar.setValue(value)
+    
+    def update_status(self, message):
+        self.status_label.setText(message)
+        self.log_text.append(message)
+    
+    def on_installation_complete(self):
+        self.status_label.setText("Instalación completada")
+        self.progress_bar.setValue(100)
+        self.title.setText("Instalado")
+        self.finish_button.setVisible(True)
+        # Iniciar el temporizador para cerrar la ventana después de 5 segundos
+        # self.close_timer.start(5000)  # 5000 milisegundos = 5 segundos
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.is_dragging = True
+            self.drag_position = event.globalPos() - self.pos()
+
+    def mouseMoveEvent(self, event):
+        if self.is_dragging:
+            self.move(event.globalPos() - self.drag_position)
+
+    def mouseReleaseEvent(self, event):
+        self.is_dragging = False
+
+# Inicio del programa
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    icon = QIcon(resource_path("icon.png"))
+    app.setWindowIcon(icon)
+
+    window = GameInstallationProgress()
+    window.setWindowIcon(icon)
+    window.show()
+    sys.exit(app.exec_())
