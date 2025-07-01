2379ec98683927af62483884178b1fd972237e07
ysalexg <alexgcord@proton.me>
2025-06-30 22:31:17 -0300

    better releases

--- compiler/release.py
+++ compiler/release.py
@@ -18,6 +18,11 @@ print(result.stdout)
 if result.returncode != 0:
     print("Error:", result.stderr)
 
+
+# git remote remove origin
+# git remote add origin https://github.com/ysalexg/zerokey.git
 # git tag -a v1.0.0 -m "Primera versión"
 # git push origin v1.0.0
 # gh release create v1.0.0 zerokey.7z zerokeyInstaller.exe --title "v1.0.0" --notes "Primera versión"
+# git remote remove origin
+# git remote add origin https://github.com/ysalexg/zerokey-dev.git
